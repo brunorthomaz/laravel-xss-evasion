6f30e1414886a90da17f60dcb188b6dd458a18cc
Bruno Thomaz <brunorthomaz@gmail.com>
2018-06-28 22:14:17 -0300

    Aplicando regras de evasão ao middleware

--- src/EvasionMiddleware.php
+++ src/EvasionMiddleware.php
@@ -3,19 +3,41 @@
 namespace XSSEvasion;
 
 use Closure;
+use XSSEvasion\EvasionFilter;
 
 class EvasionMiddleware
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @var  \XSSEvasion\EvasionFilter
+     */
+    protected $evasionFilter;
+
+    /**
+     * EvasionMiddleware constructor.
+     * @param \XSSEvasion\EvasionFilter $evasionFilter
+     */
+    public function __construct(EvasionFilter $evasionFilter)
+    {
+        $this->evasionFilter = $evasionFilter;
+    }
+
+    /**
+     * @param $request
+     * @param Closure $next
      * @return mixed
+     * @throws \XSSEvasion\Exceptions\FilterException
      */
     public function handle($request, Closure $next)
     {
-        // todo
+        $input = $request->input();
+
+        foreach($input as $key => $value) {
+            if(is_string($value)) {
+                $input[$key] = $this->evasionFilter->filter($value);
+            }
+        }
+
+        $request->merge($input);
 
         return $next($request);
     }
